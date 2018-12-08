7290616f426292480402331d6834d1d4172b1679
Erik Östlund <erik.ostlund@gmail.com>
2018-12-09 00:56:53 +0100

    Removed unused imports

--- day3/java/Main.java
+++ day3/java/Main.java
@@ -1,9 +1,6 @@
 package com.example;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.regex.Pattern;
