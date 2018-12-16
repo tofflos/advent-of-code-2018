8bc4ab4cfebbb019f30d7dfdc6a22c050cd49473
Erik Östlund <erik.ostlund@gmail.com>
2018-12-16 11:44:50 +0100

    Added solution for day 14 part 1

---
+++ day14/js/app.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const part1 = (rounds) => {
+    const recipes = [3, 7];
+    let elf1 = 0, elf2 = 1;
+
+    for (let i = 0; i < rounds + 10; i++) {
+        var recipe = recipes[elf1] + recipes[elf2];
+        var digits = Array.from(recipe.toString()).map(Number);
+
+        recipes.push(...digits);
+
+        elf1 = (elf1 + recipes[elf1] + 1) % recipes.length;
+        elf2 = (elf2 + recipes[elf2] + 1) % recipes.length;
+    }
+
+    return recipes.slice(rounds, rounds + 10);
+}
+
+console.log("Part one: " + part1(157901).join(""))
