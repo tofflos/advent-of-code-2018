7d28e66903d02a11ee0198ecad9af5275ca4c176
Erik Östlund <erik.ostlund@gmail.com>
2018-12-21 19:19:50 +0100

    Added solution for day 10

--- day10/js/app.js
+++ day10/js/app.js
@@ -11,29 +11,62 @@ class Point {
     }
 }
 
-const points = fs.readFileSync("day10/js/examples1.in").toString().split("\r\n")
+let points = fs.readFileSync("day10/js/10.in").toString().split("\r\n")
     .map(line => pattern.exec(line))
     .map(matches => {
         return new Point(Number(matches[1]), Number(matches[2]), Number(matches[3]), Number(matches[4]))
     });
 
-for (let n = 0; n < 5; n++) {
-    console.log(`${n} seconds: `)
 
-    const minX = Math.min(...points.map(p => p.x));
-    const maxX = Math.max(...points.map(p => p.x));
-    const minY = Math.min(...points.map(p => p.y));
-    const maxY = Math.max(...points.map(p => p.y));
+const smallestSky = (stars, maximumWait) => {
+    let smallestArea = Infinity;
+    let smallestSecond = -1;
+
+    for (let i = 0; i < maximumWait; i++) {
+        const minX = Math.min(...stars.map(p => p.x));
+        const maxX = Math.max(...stars.map(p => p.x));
+        const minY = Math.min(...stars.map(p => p.y));
+        const maxY = Math.max(...stars.map(p => p.y));
+
+        const height = maxY - minY + 1;
+        const width = maxX - minX + 1;
+        const area = height * width;
+
+        if (area < smallestArea) {
+            smallestArea = area;
+            smallestSecond = i;
+        }
+
+        stars.forEach(p => {
+            p.x = p.x + p.dx;
+            p.y = p.y + p.dy;
+        });
+    }
+
+    return smallestSecond;
+}
+
+const renderSky = (stars, second) => {
+    stars.forEach(p => {
+        p.x = p.x + second * p.dx;
+        p.y = p.y + second * p.dy;
+    });
+
+    const minX = Math.min(...stars.map(p => p.x));
+    const maxX = Math.max(...stars.map(p => p.x));
+    const minY = Math.min(...stars.map(p => p.y));
+    const maxY = Math.max(...stars.map(p => p.y));
 
     const height = maxY - minY + 1;
     const width = maxX - minX + 1;
+
     const sky = Array(height).fill(0).map(() => Array(width).fill("."));
 
     const transpose = (p) => {
         return new Point(p.x - minX, p.y - minY, p.dx, p.dy);
     }
 
-    points.map(transpose).forEach(p => {
+    stars.map(transpose).forEach(p => {
         sky[p.y][p.x] = "#";
     });
 
@@ -44,10 +77,17 @@ for (let n = 0; n < 5; n++) {
         }
         console.log(row);
     }
-    console.log("");
-
-    points.forEach(p => {
-        p.x = p.x + p.dx;
-        p.y = p.y + p.dy;
-    });
 }
+
+const probablyTheRightTime = smallestSky(points, 5 * 3600);
+
+points = fs.readFileSync("day10/js/10.in").toString().split("\r\n")
+    .map(line => pattern.exec(line))
+    .map(matches => {
+        return new Point(Number(matches[1]), Number(matches[2]), Number(matches[3]), Number(matches[4]))
+    });
+
+console.log("Part one:");
+renderSky(points, probablyTheRightTime);
+console.log("");
+console.log("Part two: " + probablyTheRightTime);
