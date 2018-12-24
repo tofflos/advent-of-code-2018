5172bc9a6d0ee38a9526b9e5c5aa7f1679763532
Erik Östlund <erik.ostlund@gmail.com>
2018-12-24 23:15:28 +0100

    Added solution for day 2

--- day12/java/Main.java
+++ day12/java/Main.java
@@ -5,23 +5,24 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         var iterator = Files.readAllLines(Paths.get("12.in")).iterator();
         var initialState = iterator.next().substring("initial state: ".length());
-        var notes = new HashMap<String, Character>();
 
+        var notes = new HashMap<String, Character>();
         iterator.next();
         iterator.forEachRemaining(s -> notes.put(s.substring(0, 5), s.charAt(s.length() - 1)));
 
         System.out.println("Part one: " + part1(notes, initialState));
+        System.out.println("Part two: " + part2(notes, initialState));
     }
 
-    static int part1(HashMap<String, Character> notes, String initialState) {
+    static long part1(HashMap<String, Character> notes, String initialState) {
         var bufferSize = 1024;
+        var negativeBufferSize = 5;
         char currentPots[] = new char[bufferSize];
 
         Arrays.fill(currentPots, '.');
@@ -30,7 +31,7 @@ public class Main {
         for (long n = 0; n < 20; n++) {
             var nextPots = Arrays.copyOf(currentPots, bufferSize);
 
-            for (int i = -5; i < currentPots.length - 5; i++) {
+            for (int i = -negativeBufferSize; i < bufferSize - negativeBufferSize; i++) {
                 var sb = new StringBuilder();
                 for (int j = 0; j < 5; j++) {
                     sb.append(currentPots[Math.floorMod(i + j, bufferSize)]);
@@ -44,14 +45,61 @@ public class Main {
             System.arraycopy(nextPots, 0, currentPots, 0, bufferSize);
         }
 
-        var count = 0;
+        return calculateScore(currentPots, negativeBufferSize);
+    }
 
-        for (int i = -5; i < currentPots.length - 5; i++) {
-            if (currentPots[Math.floorMod(i, currentPots.length)] == '#') {
-                count += i;
+    static long part2(HashMap<String, Character> notes, String initialState) {
+        var bufferSize = 512;
+        var negativeBufferSize = 5;
+        var currentPots = new char[bufferSize];
+
+        Arrays.fill(currentPots, '.');
+        System.arraycopy(initialState.toCharArray(), 0, currentPots, 0, initialState.length());
+
+        long generation = 0;
+        Long previous = null;
+        Long current = null;
+        Long next = null;
+
+        while (true) {
+            var nextPots = Arrays.copyOf(currentPots, bufferSize);
+
+            for (int i = -negativeBufferSize; i < bufferSize - negativeBufferSize; i++) {
+                var sb = new StringBuilder();
+                for (int j = 0; j < 5; j++) {
+                    sb.append(currentPots[Math.floorMod(i + j, bufferSize)]);
+                }
+
+                var currentString = sb.toString();
+
+                nextPots[Math.floorMod(i + 2, bufferSize)] = notes.getOrDefault(currentString, '.');
+            }
+
+            previous = current;
+            current = calculateScore(currentPots, negativeBufferSize);
+            next = calculateScore(nextPots, negativeBufferSize);
+
+            if (previous != null && current - previous == next - current) {
+                break;
+            }
+
+            System.arraycopy(nextPots, 0, currentPots, 0, bufferSize);
+
+            generation++;
+        }
+
+        return previous + (50000000000L - (generation - 1L)) * (current - previous);
+    }
+
+    static long calculateScore(char[] arr, int negativeBufferSize) {
+        long score = 0;
+
+        for (int i = -negativeBufferSize; i < arr.length - negativeBufferSize; i++) {
+            if (arr[Math.floorMod(i, arr.length)] == '#') {
+                score += i;
             }
         }
 
-        return count;
+        return score;
     }
 }
